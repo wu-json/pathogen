361764812fdd615711895d901457500eb15118ae
wu-json <jason.wu2153@gmail.com>
2022-04-15 14:58:23 -0400

    feat: profile struct

---
+++ src/program-rust/src/profile.rs
@@ -0,0 +1,24 @@
+pub enum TestResult {
+    Positive,
+    Negative,
+}
+
+pub struct TestPoint {
+    pub test_result: TestResult,
+    pub test_date: String,
+    pub test_location: String,
+}
+
+pub struct Profile {
+    // unidentifiable biographical information
+    pub age: u8,
+    pub exercising: bool,
+    pub height: String,
+    pub occupation: String,
+    pub state_code: String,
+    pub weight: String,
+
+    // infection-specific information
+    pub infection_history: Vec<TestPoint>,
+    pub latest_test_result: TestResult,
+}
