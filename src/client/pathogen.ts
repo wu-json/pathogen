bae96922b98a0e4ad327f4eef1b908a4a17f57c2
wu-json <jason.wu2153@gmail.com>
2022-04-15 12:38:40 -0400

    feat: update establishPayer

--- src/client/pathogen.ts
+++ src/client/pathogen.ts
@@ -97,22 +97,24 @@ export async function establishConnection(): Promise<void> {
  * Establish an account to pay for everything
  */
 export async function establishPayer(): Promise<void> {
+  if (!payer) payer = await getPayer();
   let fees = 0;
-  if (!payer) {
-    const { feeCalculator } = await connection.getRecentBlockhash();
 
-    // Calculate the cost to fund the greeter account
-    fees += await connection.getMinimumBalanceForRentExemption(GREETING_SIZE);
+  // Calculate cost to fund the greeter account
+  fees += await connection.getMinimumBalanceForRentExemption(GREETING_SIZE);
 
-    // Calculate the cost of sending transactions
-    fees += feeCalculator.lamportsPerSignature * 100; // wag
+  // Calculate the cost of sending transactions
+  const { blockhash: recentBlockhash } = await connection.getLatestBlockhash();
+  const transaction = new Transaction({
+    feePayer: payer.publicKey,
+    recentBlockhash,
+  });
 
-    payer = await getPayer();
-  }
+  fees += await transaction.getEstimatedFee(connection);
 
+  // Make sure we can cover the fees
   let lamports = await connection.getBalance(payer.publicKey);
   if (lamports < fees) {
-    // If current balance is not enough to pay for fees, request an airdrop
     const sig = await connection.requestAirdrop(
       payer.publicKey,
       fees - lamports,
