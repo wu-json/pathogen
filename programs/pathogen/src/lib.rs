a72771838b5ed05d1273fc5d26e165299627bd0e
wu-json <jason.wu2153@gmail.com>
2022-04-16 15:02:26 -0400

    feat: update program id

--- programs/pathogen/src/lib.rs
+++ programs/pathogen/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("CYmfp3tVDFtfkK5TeTYbNKRT4kQa5it57jjgERaTpZwh");
 
 #[program]
 pub mod pathogen {
