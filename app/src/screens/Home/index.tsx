bc89e0f5b90948224372f81fd12dc995e5e41500
wu-json <jason.wu2153@gmail.com>
2022-04-18 16:00:35 -0400

    feat: use themes

--- app/src/screens/Home/index.tsx
+++ app/src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
 import styles from './styles.module.scss';
 
 const Home = () => {
-  return <></>;
+  return (
+    <div className={styles['page-container']}>
+      <p>hi</p>
+    </div>
+  );
 };
 
 export default Home;
