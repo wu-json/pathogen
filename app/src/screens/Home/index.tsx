ee84ef4ceddd4c5a6eeaf5ee2bf247c66be864f0
wu-json <jason.wu2153@gmail.com>
2022-04-18 16:07:41 -0400

    docs: readme

--- app/src/screens/Home/index.tsx
+++ app/src/screens/Home/index.tsx
@@ -1,9 +1,18 @@
+import PathogenLogo from '../../assets/images/pathogen_logo.png';
 import styles from './styles.module.scss';
 
 const Home = () => {
   return (
     <div className={styles['page-container']}>
-      <p>hi</p>
+      <div className={styles['header-wrapper']}>
+        <div className={styles['header-container']}>
+          <img src={PathogenLogo} />
+          <div>
+            <h1>pathogen</h1>
+            <h2>dApp for decentralized anonymous public health data</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
