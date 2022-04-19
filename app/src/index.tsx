c30a9ce2f91773391db6caace52e9bae6b195105
wu-json <jason.wu2153@gmail.com>
2022-04-18 22:23:36 -0400

    feat: polyfills

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import Wallet from './Wallet';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import './styles/themes.scss';
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Wallet>
+      <App />
+    </Wallet>
   </React.StrictMode>,
 );
 
