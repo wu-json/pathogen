83bb1cc3b94da59e26ec070de427580375365d32
wu-json <jason.wu2153@gmail.com>
2022-04-19 00:04:13 -0400

    feat: dates

--- app/src/screens/Pathogens/Pathogen/index.tsx
+++ app/src/screens/Pathogens/Pathogen/index.tsx
@@ -1,4 +1,5 @@
 import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { DateTime } from 'luxon';
 
 import SolanaCircleLogo from '../../../assets/images/solana_circle_logo.svg';
 import styles from './styles.module.scss';
@@ -8,12 +9,16 @@ type Props = {
 };
 
 const Pathogen = ({ pathogen }: Props) => {
-  console.log(pathogen);
   return (
     <div className={styles['container']}>
       <div className={styles['left-col']}>
         <h1>{pathogen.account.code}</h1>
         <h3>{pathogen.publicKey.toBase58().slice(0, 32) + '...'}</h3>
+        <h3>
+          {DateTime.fromMillis(
+            pathogen.account.createdAt.toNumber() * 1000,
+          ).toFormat('yyyy LLL dd')}
+        </h3>
       </div>
       <div className={styles['right-col']}>
         <div className={styles['button-wrapper']}>
