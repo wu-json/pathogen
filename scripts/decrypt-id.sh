aa617b5d78127438ffe8e2d9bdb13a11c322004f
wu-json <jason.wu2153@gmail.com>
2022-04-19 23:30:15 -0400

    ci: update id.json path

--- scripts/decrypt-id.sh
+++ scripts/decrypt-id.sh
@@ -1,6 +1,3 @@
-# create solana secrets directory
-mkdir -p secrets/solana
-
 # decrypt id.json
 gpg --quiet --batch --yes --decrypt --passphrase="$ID_JSON_SECRET_PASSPHRASE" \
---output secrets/solana/id.json encrypted-keypairs/id.json.gpg
+--output ~/.config/solana/id.json encrypted-keypairs/id.json.gpg
