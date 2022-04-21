ee9b2f3e71a18702e349b6ed8dcfb441e63813e4
wu-json <jason.wu2153@gmail.com>
2022-04-21 00:17:14 -0400

    feat: useCreatePathogen hook

---
+++ app/src/hooks/api/useCreatePathogen.ts
@@ -0,0 +1,51 @@
+import { web3 } from '@project-serum/anchor';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { BN } from 'bn.js';
+import { useCallback } from 'react';
+
+import useWorkspace from '../useWorkspace';
+
+const useCreatePathogen = () => {
+  const { wallet, program } = useWorkspace();
+
+  const createPathogen = useCallback(
+    async (
+      code: string,
+      name: string,
+      bounty: number,
+      rewardPerProfile: number,
+    ) => {
+      if (!wallet || !program) {
+        return null;
+      }
+
+      const pathogen = web3.Keypair.generate();
+
+      await program.methods
+        .createPathogen(
+          code,
+          name,
+          new BN(bounty * LAMPORTS_PER_SOL),
+          new BN(rewardPerProfile * LAMPORTS_PER_SOL),
+        )
+        .accounts({
+          pathogen: pathogen.publicKey,
+          creator: wallet.publicKey,
+          systemProgram: web3.SystemProgram.programId,
+        })
+        .signers([pathogen])
+        .rpc();
+
+      const pathogenAccount = await program.account.pathogen.fetch(
+        pathogen.publicKey,
+      );
+
+      return pathogenAccount;
+    },
+    [wallet, program],
+  );
+
+  return { createPathogen };
+};
+
+export default useCreatePathogen;
